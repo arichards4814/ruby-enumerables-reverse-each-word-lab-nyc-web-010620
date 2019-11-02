6bdfc498db8ce656b13dd6fe9a22fbf645088168
Andrew Richards <arichards4814@gmail.com>
2019-11-02 21:07:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,19 @@ def reverse_each_word(string)
   
   newArr = string.split
   test = []
+  i = 0;
+  string1 = ""
   
   newArr.each do |ele| 
     test.push(ele.reverse)
   end
   
-  test.to_s
+  while i < test.length do
+
+     string1 = test[i].to_s + " " + string1
+     i += 1
+  end
+  
+  string1
   
 end
